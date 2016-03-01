60ba8833638c93e42aae145765df5247c91e16d4
Steven E. Lamberson, Jr <steven.lamberson@gmail.com>
2016-02-29 21:55:40 -0600

    Turn off a few annoying emacs defaults.
    - No backups
    - No splash screen
    - No startup message

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -1,6 +1,15 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(setq vc-follow-symlinks t)
+(setq visible-bell nil)
 (setq custom-safe-themes t)
+
+(defvar backup-dir "~/.emacs.d/backups/")
+(setq backup-directory-alist (list (cons "." backup-dir)))
+(setq make-backup-files nil)
 
 (require 'init-elpa)
 (require 'init-evil)
